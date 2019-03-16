42a14180ebaf91629ccdadede43cc775ee7f9e86
Sebastian Thorsen <sebastianhoye@gmail.com>
2019-03-16 18:16:10 +0100

    f

---
+++ dicks/__init__.py
@@ -0,0 +1,4 @@
+from .dicks import Dicks
+
+def setup(bot):
+    bot.add_cog(Dicks())
---
+++ dicks/dicks.py
@@ -0,0 +1,10 @@
+from redbot.core import commands
+
+class Dicks(commands.Cog):
+    """My custom cog"""
+
+    @commands.command()
+    async def dicks(self, ctx):
+        """This does stuff!"""
+        # Your code will go here
+        await ctx.send("I can do stuff!")
