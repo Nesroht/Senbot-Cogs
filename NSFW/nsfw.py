150497d5678bd0d8b60b10bf89c5f4dfeed1847c
Sebastian Thorsen <sebastianhoye@gmail.com>
2019-03-18 15:18:54 +0100

    f

--- NSFW/nsfw.py
+++ NSFW/nsfw.py
@@ -7,6 +7,7 @@ import time
 import random
 import copy
 import json
+import urllib2
 
 from NSFW import credentials
 from bs4 import BeautifulSoup
@@ -392,7 +393,7 @@ class NSFW(commands.Cog):
         """Random Post from subreddit"""
         try:
             #postlist = self.reddit.subreddit(subreddit).hot(limit=50)
-            query = ("https://reddit.com/r/"+subreddit+"/random.json")
+            query = urllib2.urlopen("https://reddit.com/r/"+subreddit+"/random.json")
             postjson = json.load(query)
             post = postjson['data']['children']['data']
             #posts = copy.deepcopy(postlist)
