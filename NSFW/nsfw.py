f2736a3c66fc5abf3b33eca420f4bf1ec3316ab9
Sebastian Thorsen <sebastianhoye@gmail.com>
2019-03-18 20:19:14 +0100

    f

--- NSFW/nsfw.py
+++ NSFW/nsfw.py
@@ -544,7 +544,8 @@ class NSFW(commands.Cog):
             self.authorize(self)
             query = requests.get("https://oauth.reddit.com/r/" + subreddit + "/random.json", headers=self.headers)
         postin = query.json()
-        postin = postin[0]
+        if postin is list:
+            postin = postin[0]
         postjson = postin.get("data")
         postjson = postjson.get("children")
         postjson = postjson[0]
