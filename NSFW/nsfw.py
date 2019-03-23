ca38a7c1cc67969f7cd5286c13dbdbbc7728ae69
Sebastian Thorsen <sebastianhoye@gmail.com>
2019-03-23 17:21:23 +0100

    Attempt at reactivating e621

--- NSFW/nsfw.py
+++ NSFW/nsfw.py
@@ -622,7 +622,7 @@ class NSFW(commands.Cog):
         try:
             query = ("https://e621.net/post/random")
             page = await (await self._session.get(query)).text()
-            print(page)
+            await ctx.send("```page```")
             soup = BeautifulSoup(page, "html.parser")
             image = soup.find(id="highres").get("href")
             emb = discord.Embed(title="e621")
