ccd62ed741e262d60781b1b05b8f1ef1f27fd091
Sebastian Thorsen <sebastianhoye@gmail.com>
2019-03-23 17:27:28 +0100

    Need to pass a user-agent for e621 apparently

--- NSFW/nsfw.py
+++ NSFW/nsfw.py
@@ -621,7 +621,7 @@ class NSFW(commands.Cog):
         """Random Image From e621"""
         try:
             query = ("https://e621.net/post/random")
-            page = await (await self._session.get(query)).text()
+            page = await (await self._session.get(query, headers={"User-Agent":"Senbot discord bot (by Senbot on e621)"})).text()
             await ctx.send("```"+page+"```")
             soup = BeautifulSoup(page, "html.parser")
             image = soup.find(id="highres").get("href")
