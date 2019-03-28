c6cff79b6c4338b15f9091890e8dbbab4295a38d
Sebastian <sebastianhoye@gmail.com>
2019-03-28 00:56:22 +0000

    Try this

--- twitchbot/twitchbot.py
+++ twitchbot/twitchbot.py
@@ -13,7 +13,7 @@ class Twitchbot(commands.Cog):
         self.credentials = credentials
         self.helix = twitch.Helix(credentials.CLIENT_ID)
         self.channels = {"nesroht"}
-        self.messageHandler()
+        self.messageListener()
 
 
     @commands.command()
