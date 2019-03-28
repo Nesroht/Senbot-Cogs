9031a4d3599fd43b6cec6775f5bf8020262d1213
Sebastian <sebastianhoye@gmail.com>
2019-03-28 00:42:47 +0000

    Try this

--- twitchbot/twitchbot.py
+++ twitchbot/twitchbot.py
@@ -20,10 +20,20 @@ class Twitchbot(commands.Cog):
         try:
             await self.messageHandler(channel)
             twitch.Chat(channel='#' + channel, nickname=self.credentials.CLIENT_USERNAME,
-                        oauth=credentials.CLIENT_OAUTH).send("Testing connection to twitch")
+                        oauth=credentials.CLIENT_OAUTH).send("Ping!")
         except Exception as e:
             await ctx.send(e)
 
-    async def messageHandler(self, channel):
-        twitch.Chat(channel='#'+channel, nickname=self.credentials.CLIENT_USERNAME, oauth=self.credentials.CLIENT_OAUTH).subscribe(
-            lambda message: print(message.channel, message.sender, message.text))
+    async def messageListener(self):
+        for channel in self.channels:
+            twitch.Chat(channel='#'+channel, nickname=self.credentials.CLIENT_USERNAME, oauth=self.credentials.CLIENT_OAUTH).subscribe(
+                lambda message: self.messageHandler(message))
+
+
+    async def messageHandler(self, message):
+        user = message.helix.user(message.sender).display_name
+        if message.helix.user(message.sender).displayname is not self.credentials.CLIENT_USERNAME:
+            print(message.channel, user, message.text)
+        else:
+            if (message.text is "Ping!") and (user is self.credentials.CLIENT_USERNAME):
+                print("Pong!")
