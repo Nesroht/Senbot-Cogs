14fd2adf3b672b5db6c63499ffc1ad92199af02e
Sebastian <sebastianhoye@gmail.com>
2019-04-24 21:02:18 +0200

    ok this should do

--- twitchbot/twitchbot.py
+++ twitchbot/twitchbot.py
@@ -168,7 +168,7 @@ class Twitchbot(commands.Cog):
                     now = round(time.time())
                     if "!join\r\n" in senderdata:
                         print("yep that works")
-                        if "1uptaco" in channel:
+                        if "nesroht" in channel:
                             print("pass")
                         else:
                             continue
