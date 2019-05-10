f06f7cfca5936d160a32b709c5a526a2f5587e42
Sebastian <sebastianhoye@gmail.com>
2019-05-10 19:14:36 +0200

    add sourcecode command to utils

--- utils/utils.py
+++ utils/utils.py
@@ -2,6 +2,8 @@ import discord
 import time
 import aiohttp
 from redbot.core import checks, Config, commands
+from redbot.core.utils.chat_formatting import pagify
+from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
 
 
 
@@ -39,3 +41,25 @@ class Utils(commands.Cog):
         emb.add_field(name="Users", value="```" + users + "```")
         emb.add_field(name="Servers", value="```" + servers + "```")
         await ctx.send(embed=emb)
+
+    @commands.command()
+    @checks.is_owner()
+    async def sourcecode(ctx, *, command: str):
+        """
+        Get the source code of a command
+        """
+        command = bot.get_command(command)
+        if command is None:
+            await ctx.send("Command not found.")
+            return
+        source_code = inspect.getsource(command.callback)
+        temp_pages = []
+        pages = []
+        for page in pagify(source_code, escape_mass_mentions=True, page_length=1980):
+            temp_pages.append("py\n" + page + " ")
+        max_i = len(temp_pages)
+        i = 1
+        for page in temp_pages:
+            pages.append(f"Page {i}/{max_i}\n" + page)
+            i += 1
+        await menu(ctx, pages, controls=DEFAULT_CONTROLS)
