457683b136f17e8ae5fee9dbec580d2f236cd881
Sebastian Høyer Thorsen <sebast14@student.uia.no>
2020-01-20 17:43:56 +0100

    fix?

--- neko/neko.py
+++ neko/neko.py
@@ -13,6 +13,9 @@ import nekos
 
 class Neko(commands.Cog):
     """Neko commands for Senbot"""
+    NSFW = ['feet', 'yuri', 'trap', 'futanari', 'hololewd', 'lewdkemo', 'solog', 'feetg', 'cum', 'erokemo', 'les', 'wallpaper', 'lewdk', 'lewd', 'gecg', 'eroyuri', 'eron', 'cum_jpg', 'bj', 'nsfw_neko_gif', 'solo', 'nsfw_avatar', 'gasm', 'anal', 'hentai', 'erofeet', 'keta', 'blowjob', 'pussy', 'tits', 'holoero', 'pussy_jpg', 'pwankg', 'classic', 'kuni', 'femdom', 'spank', 'erok', 'fox_girl', 'boobs', 'random_hentai_gif', 'smallboobs', 'ero']
+    SFW = ['tickle','poke','kiss','slap','cuddle','hug','pat','smug','feed','ngif','kemonomimi','neko']
+    RANDOM = ['lizard','avatar','holo','waifu','8ball','goose']
 
     def __init__(self, bot):
         self.bot = bot
@@ -20,10 +23,21 @@ class Neko(commands.Cog):
 
     @commands.command()
     async def neko(self, ctx, *, str):
-        """Reply with latency of bot"""
+        """Ask the Neko api for picture"""
         try:
-            emb = discord.Embed(title="Have some " + str.capitalize(), color=discord.Color.red())
-            emb.set_image(url=nekos.img(str))
-            await ctx.send(embed=emb)
+            if ctx.channel.isnsfw() == True:
+                if str in (NSFW or SFW or RANDOM):
+                    emb = discord.Embed(title="Have some " + str.capitalize(), color=discord.Color.pink())
+                    emb.set_image(url=nekos.img(str))
+                    await ctx.send(embed=emb)
+                else:
+                    await ctx.send(ctx.box("Nope"))
+            else:
+                if str in (SFW or RANDOM):
+                    emb = discord.Embed(title="Have some " + str, color=discord.Color.pink())
+                    emb.set_image(url=nekos.img(str))
+                    await ctx.send(embed=emb)
+                else:
+                    await ctx.send(ctx.box("Nope"))
         except Exception as e:
             await ctx.send(f":x: **Error:** `{e}`")
